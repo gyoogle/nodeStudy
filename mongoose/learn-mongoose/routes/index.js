12a651c58ef2d128365a7766527dda3868715dbf
gyusuk <gyusuk6394@gmail.com>
2018-10-08 16:44:41 +0900

    nodejs-mongoose

---
+++ mongoose/learn-mongoose/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var User = require('../schemas/user');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  User.find({})
+    .then((users) => {
+      res.render('mongoose', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
+});
+
+/*
+router.get('/', async (req, res, next) => {
+  try{
+    const users = await User.find();
+    res.render('mongoose', { users });
+  }catch(error){
+    console.error(error);
+    next(error);
+  }
+});
+*/
+
+module.exports = router;
